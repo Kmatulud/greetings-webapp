6f34f65d546552c4521cf8f26a1044e23626b4f0
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-08-18 10:31:43 +0200

    express-flash

--- views/client.js
+++ views/client.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    let greetMsg = document.querySelector(".greeting");
     let errorMsgElem = document.querySelector('.error');
     if (errorMsgElem.innerHTML !== ''){
+        greetMsg.innerHTML = '';
         setTimeout(function(){
             errorMsgElem.innerHTML = '';
         },3000)
