abdfba6a1af6823848c6fd719026c5f39fd35abd
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-09-27 16:49:37 +0200

    added routes folder

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var dotenv = require("dotenv").config();
+var GreetMe = require("./routes/routes")
 const flash = require("express-flash");
 const session = require("express-session");
-const Greetings = require("./greetings");
-const pg = require("pg");
-const Pool = pg.Pool;
 
-// should we use a SSL connection
-let useSSL = false;
-let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) {
-	useSSL = true;
-}
-// which db connection to use
-const connectionString =
-	process.env.DATABASE_URL ||
-	"postgresql://postgres:3201@localhost:5432/greetingswebapp";
 
-const pool = new Pool({
-	connectionString,
-	ssl: useSSL,
-});
+let greetMe = GreetMe();
 
 const app = express();
-const greetings = Greetings();
 
 app.use(express.static("public"));
 app.use(express.static("views"));
@@ -52,81 +37,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parse application/json
 app.use(bodyParser.json());
 
-app.get("/", async function(req, res){
-	var data = await pool.query('select * from users')
-	var newdata = data.rows[data.rows.length-1]
-	var names = [];
-	data.rows.forEach((obj)=>{
-		if(names.indexOf(obj.user_name) === -1){
-			names.push(obj.user_name)
-		}else{
-
-		}
-	})
-	try {
-			res.render("index", {
-				getTheName: newdata.user_name,
-				language: newdata.greet_msg,
-				// getGreetMessage: greetings.getGreetMessage(),
-				countGreetedNames: names.length,
-			});
-	} catch (error) {
-		 res.render("index", {countGreetedNames: names.length});
-
-	}
-
-
-});
-app.get("/greeted", async function(req, res){
-	var data = await pool.query('select * from users')
-	var newdata = data.rows
-	console.log(data.rows)
-	var names = [];
-	
-	try {
-		res.render("greeted", {
-			nameList: newdata,
-			getTheName: newdata.user_name,
-			language: newdata.greet_msg,
-		});
-	} catch (error) {
-		res.render("greeted");
-	}
-});
-app.post("/greetings", async function(req, res){
-	greetings.setTheName(req.body.name);
-	greetings.setLanguage(req.body.language);
-
-	greetings.setGreetMessage();
-	let greetMsg = greetings.getGreetMessage();
-
-	let count = greetings.countGreetedNames();
-	let getTheName = greetings.getTheName();
-
-	let language = greetings.getLanguage();
-	if (getTheName && getTheName !== "") {
-		console.log(getTheName, greetMsg, count);
-		await pool.query(
-			'insert into users (user_name, greet_msg, counter) values ($1, $2, $3)',
-			[getTheName, greetMsg, count]
-		)
-	}
-	if (getTheName === "") {
-		req.flash("error", "Please enter your name!");
-	} else if (language === undefined) {
-		req.flash("error", "Please choose a language!");
-	}
-
-	
-	greetings.setNamesGreeted(req.body.name);
-	res.redirect("/");
-});
-app.get('/delete', async function(req, res){
-		await pool.query(
-			'delete from users'
-		)
-	res.redirect('/');
-})
+app.get("/", greetMe.home);
+app.get("/greeted", greetMe.greeted);
+app.get("/counter", greetMe.greetedCount);
+app.post("/greetings", greetMe.greetMsg);
+app.get("/delete", greetMe.deleteUsers);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/routes.js
@@ -0,0 +1,115 @@
+const Greetings = require("../greetings");
+const greetings = Greetings();
+const pg = require("pg");
+const Pool = pg.Pool;
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+	useSSL = true;
+}
+// which db connection to use
+const connectionString =
+	process.env.DATABASE_URL ||
+	"postgresql://postgres:3201@localhost:5432/greetingswebapp";
+const pool = new Pool({
+	connectionString,
+	ssl: useSSL,
+});
+
+module.exports = function GreetMe() {
+	async function home(req, res) {
+		var data = await pool.query("select * from users");
+		var newdata = data.rows[data.rows.length - 1];
+		var names = [];
+		data.rows.forEach((obj) => {
+			if (names.indexOf(obj.user_name) === -1) {
+				names.push(obj.user_name);
+			} else {
+			}
+		});
+		try {
+			res.render("index", {
+				getTheName: newdata.user_name,
+				language: newdata.greet_msg,
+				countGreetedNames: names.length,
+			});
+		} catch (error) {
+			res.render("index", { countGreetedNames: names.length });
+		}
+	}
+	let greeted = async function (req, res) {
+		var data = await pool.query("select * from users");
+		var newdata = data.rows;
+
+		try {
+			res.render("greeted", {
+				nameList: newdata,
+			});
+		} catch (error) {
+			res.render("greeted");
+		}
+	};
+	async function greetedCount(req, res) {
+		var data = await pool.query("select * from users");
+		var newdata = data.rows;
+		try {
+			res.render("counter", {
+				namesList: newdata,
+			});
+		} catch (error) {
+			res.render("counter");
+		}
+	}
+
+	async function greetMsg(req, res) {
+		greetings.setTheName(req.body.name);
+		greetings.setLanguage(req.body.language);
+
+		greetings.setGreetMessage();
+		let greetMsg = greetings.getGreetMessage();
+
+		// let count = greetings.countGreetedNames();
+		let getTheName = greetings.getTheName();
+
+		let language = greetings.getLanguage();
+		var checkName = await pool.query(
+			"select user_name from users where user_name = $1",
+			[getTheName]
+		);
+		if (checkName.rowCount < 1) {
+			await pool.query(
+				"insert into users(user_name, greet_msg, counter)values($1,$2,$3)",
+				[getTheName, greetMsg, 1]
+			);
+		} else {
+			await pool.query(
+				"update users set counter=counter+1 where user_name = $1",
+				[getTheName]
+			);
+		}
+
+		greetings.setNamesGreeted(req.body.name);
+		res.redirect("/");
+	}
+
+	async function deleteUsers(req, res) {
+		await pool.query("delete from users");
+		res.redirect("/");
+	}
+    async function validName(){
+        if (getTheName === "") {
+			req.flash("error", "Please enter your name!");
+		} else if (language === undefined) {
+			req.flash("error", "Please choose a language!");
+			}
+        }
+    return {
+			home,
+			greeted,
+			greetedCount,
+			greetMsg,
+			deleteUsers,
+			validName,
+		};
+};
